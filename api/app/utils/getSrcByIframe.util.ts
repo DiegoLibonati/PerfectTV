46d26bb46ac01d21ec5a9c062e5588c063779441
Die <dlibonati@leafnoise.io>
2025-05-09 13:31:31 -0300

    feat: logs to test in server

--- api/app/utils/getSrcByIframe.util.ts
+++ api/app/utils/getSrcByIframe.util.ts
@@ -5,48 +5,51 @@ import { config } from "@app/config/env.conf";
 import { resolveFinalUrl } from "@app/utils/resolveFinalUrl";
 
 export const getSrcByIframe = async (url: string): Promise<string> => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath: config.PUPPETEER_EXECUTABLE_PATH || undefined,
-  });
+  const finalUrl = await resolveFinalUrl(url);
+  console.log("FINAL URL: ", finalUrl)
+  return "a"
+  // const browser = await puppeteer.launch({
+  //   headless: true,
+  //   args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  //   executablePath: config.PUPPETEER_EXECUTABLE_PATH || undefined,
+  // });
 
-  try {
-    const page = await browser.newPage();
-    const finalUrl = await resolveFinalUrl(url);
+  // try {
+  //   const page = await browser.newPage();
+  //   const finalUrl = await resolveFinalUrl(url);
 
-    await page.goto(finalUrl, { waitUntil: "networkidle2", timeout: 0 });
+  //   await page.goto(finalUrl, { waitUntil: "networkidle2", timeout: 0 });
 
-    // Esperar a que el iframe esté en el DOM
-    const iframeElementHandle = await page.waitForSelector("iframe");
+  //   // Esperar a que el iframe esté en el DOM
+  //   const iframeElementHandle = await page.waitForSelector("iframe");
 
-    // Obtener el iframe como un frame de Puppeteer
-    const iframe = await iframeElementHandle?.contentFrame();
+  //   // Obtener el iframe como un frame de Puppeteer
+  //   const iframe = await iframeElementHandle?.contentFrame();
 
-    // Esperar que el `src` del iframe esté definido
-    await page.waitForFunction(
-      (iframeSelector) => {
-        const iframe = document.querySelector(
-          iframeSelector
-        ) as HTMLIFrameElement;
-        return iframe && iframe.src && iframe.src.trim();
-      },
-      {}, // Opciones de espera (vacío para usar valores por defecto)
-      "iframe" // Selector del iframe
-    );
+  //   // Esperar que el `src` del iframe esté definido
+  //   await page.waitForFunction(
+  //     (iframeSelector) => {
+  //       const iframe = document.querySelector(
+  //         iframeSelector
+  //       ) as HTMLIFrameElement;
+  //       return iframe && iframe.src && iframe.src.trim();
+  //     },
+  //     {}, // Opciones de espera (vacío para usar valores por defecto)
+  //     "iframe" // Selector del iframe
+  //   );
 
-    // Obtener el `src` del iframe
-    const iframeSrc = await page.evaluate(
-      (iframeSelector) =>
-        (document.querySelector(iframeSelector) as HTMLIFrameElement)!.src,
-      "iframe" // Selector del iframe
-    );
+  //   // Obtener el `src` del iframe
+  //   const iframeSrc = await page.evaluate(
+  //     (iframeSelector) =>
+  //       (document.querySelector(iframeSelector) as HTMLIFrameElement)!.src,
+  //     "iframe" // Selector del iframe
+  //   );
 
-    return iframeSrc;
-  } catch (error) {
-    console.error("Error al obtener el iframe src:", error);
-    throw error;
-  } finally {
-    await browser.close();
-  }
+  //   return iframeSrc;
+  // } catch (error) {
+  //   console.error("Error al obtener el iframe src:", error);
+  //   throw error;
+  // } finally {
+  //   await browser.close();
+  // }
 };
--- api/app/utils/resolveFinalUrl.ts
+++ api/app/utils/resolveFinalUrl.ts
@@ -1,17 +1,20 @@
-import { https } from 'follow-redirects';
+import { https } from "follow-redirects";
 
 export const resolveFinalUrl = (initialUrl: string): Promise<string> => {
+  console.log("entrando en funcion resolveFinalUrl");
   return new Promise((resolve, reject) => {
-    https.get(initialUrl, (res) => {
-      if (res.responseUrl) {
-        console.log("✅ Final URL:", res.responseUrl);
-        resolve(res.responseUrl);
-      } else {
-        reject("No se pudo obtener la URL final");
-      }
-    }).on('error', (err) => {
-      console.error("❌ Error:", err.message);
-      reject(err);
-    });
+    https
+      .get(initialUrl, (res) => {
+        if (res.responseUrl) {
+          console.log("✅ Final URL:", res.responseUrl);
+          resolve(res.responseUrl);
+        } else {
+          reject("No se pudo obtener la URL final");
+        }
+      })
+      .on("error", (err) => {
+        console.error("❌ Error:", err.message);
+        reject(err);
+      });
   });
 };
