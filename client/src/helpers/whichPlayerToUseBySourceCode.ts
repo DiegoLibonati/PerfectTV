e4364f70c3979fbd0643d42e7a06b983cc99d6a7
Die <dlibonati@leafnoise.io>
2025-05-16 15:33:13 -0300

    fix: fix url youtube when is a live video

--- client/src/helpers/whichPlayerToUseBySourceCode.ts
+++ client/src/helpers/whichPlayerToUseBySourceCode.ts
@@ -1,7 +1,7 @@
 export const whichPlayerToUseBySourceCode = (
   sourceCode: string
 ): "iframe" | "react-player" => {
-  const useIframe = ["ftv", "vmfedgeapps"];
+  const useIframe = ["ftv", "vmfedgeapps", "youtube"];
   const code = sourceCode.toLowerCase();
 
   if (useIframe.includes(code)) return "iframe";
